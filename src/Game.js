98e9ed1067ad9033e33fc7f03a22259b845a8be2
Florencia Rumpel Rodriguez <rumpelcita@gmail.com>
2016-01-30 20:38:24 -0300

    Pretty fonts

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,5 @@
 function create(){
-    game.stage.backgroundColor = '#ffffff';
+    background = game.add.tileSprite(0, 0, 402, 626, "background");
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     player = game.add.sprite(game.world.width/2, game.world.height - 230, 'fairy');
@@ -12,18 +12,17 @@ function create(){
     player.animations.add('left', [3,4,3], 14, false);
     player.animations.play('center');
 
-    player.current_health = 125;
-    player.max_health = 125;
-    health_text = game.add.text(16, 16, 'health:' + player.current_health + '/' + player.max_health, { fontSize: '32px', fill: '#000' });
-    player.current_lives = 3;
-    player.max_lives = 3;
-    lives_text = game.add.text(275, 16, 'lives:' + player.current_lives, { fontSize: '32px', fill: '#000' });
+    player.current_health = 75;
+    player.max_health = 75;
+    health_tooltip = game.add.text(42, 10, 'health:', { font: "bold 32px brain_flowerregular", fill: '#000' });
+    health_text = game.add.text(115, 10, player.current_health + '/' + player.max_health, { font: "bold 32px brain_flowerregular", fill: '#ff0707' });
+    health_sprite = game.add.sprite(16, 16, 'heart');
 
     spells = game.add.group();
     game.physics.enable(spells);
     spells.max = 9;
     spells.enableBody = true;
-    spells_speed = 125;
+    spells_speed = 80;
 
     timer = game.time.create(false);
     timer.loop(800, spawnSpells, this);
@@ -33,6 +32,8 @@ function create(){
 }
 
 function update(){
+    background.tilePosition.y += 2;
+
     spells.forEach(function(spell){
         if (spell.inWorld == false){
             spell.kill();
@@ -63,7 +64,6 @@ function update(){
 }
 
 function render(){
-    game.debug.text('Time until event: ' + timer.duration.toFixed(0), 32, 32);
 }
 
 function menu(){
@@ -78,9 +78,31 @@ function spawnSpells(){
         4 : 'attack',
         5 : 'attack',
     }
+    var heal_spells = {
+        1 : 'heal_1',
+        2 : 'heal_2',
+        3 : 'heal_3',
+    }
+    var attack_spells = {
+        1 : 'attack_1',
+        2 : 'attack_2',
+        3 : 'attack_3',
+    }
+    var buff_spells = {
+        1 : 'buff_1',
+        2 : 'buff_2',
+        3 : 'buff_3',
+    }
+
     if (spells.countLiving() < spells.max) {
         var spell_type = game.rnd.integerInRange(1, 5);
-        var spell = game.add.sprite(game.rnd.integerInRange(14, 400), game.world.height - 600, spell_types[spell_type]);
+        if (spell_types[spell_type] == 'heal'){
+            var spell = game.add.sprite(game.rnd.integerInRange(5, 272), game.world.height - 600, heal_spells[game.rnd.integerInRange(1, 3)]);
+        } else if (spell_types[spell_type] == 'attack'){
+            var spell = game.add.sprite(game.rnd.integerInRange(5, 272), game.world.height - 600, attack_spells[game.rnd.integerInRange(1, 3)]);
+        } else if (spell_types[spell_type] == 'buff'){
+            var spell = game.add.sprite(game.rnd.integerInRange(5, 272), game.world.height - 600, buff_spells[game.rnd.integerInRange(1, 3)]);
+        }
         spell.spell_type = spell_types[spell_type];
         game.physics.enable(spell);
         spell.body.gravity.y = spells_speed;
@@ -100,25 +122,13 @@ function fairyStatus(player, spell){
             player.current_health = 0;
         }
     } else if (spell.spell_type == 'buff') {
-        if (player.current_lives < player.max_lives){
-            player.current_lives += 1;
-            lives_text.text = 'lives:' + player.current_lives;
-        } else {
             spells_speed = Math.floor(spells_speed/2);
-        }
     }
-    health_text.text = 'health:' + player.current_health + '/' + player.max_health;
+    health_text.text = player.current_health + '/' + player.max_health;
     spell.kill();
     if (player.current_health <= 0){
-        player.current_lives -= 1;
-        lives_text.text = 'lives:' + player.current_lives;
-        if (player.current_lives == 0){
-            game.paused = true;
-            console.log('game_end');
-        } else {
-            player.current_health = 125;
-            health_text.text = 'health:' + player.current_health + '/' + player.max_health;
-        }
+        game.paused = true;
+        console.log('game_end');
     }
 }
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,15 +1,23 @@
 function preload() {
     game.load.image('play', 'assets/img/play.png');
     game.load.spritesheet('fairy', 'assets/img/fairy.png', 128, 128);
-    game.load.image('heal', 'assets/img/heal.png');
-    game.load.image('attack', 'assets/img/attack.png');
-    game.load.image('buff', 'assets/img/buff.png');
+    game.load.image('heal_1', 'assets/img/items/muffin.png');
+    game.load.image('heal_2', 'assets/img/items/caramelos.png');
+    game.load.image('heal_3', 'assets/img/items/chupetin.png');
+    game.load.image('attack_1', 'assets/img/items/edesur.png');
+    game.load.image('attack_2', 'assets/img/items/manaos.png');
+    game.load.image('attack_3', 'assets/img/items/pitusas.png');
+    game.load.image('buff_1', 'assets/img/items/gatobeja.png');
+    game.load.image('buff_2', 'assets/img/items/gatomice.png');
+    game.load.image('buff_3', 'assets/img/items/gatotaza.png');
+    game.load.image('background', 'assets/img/fondo.png');
+    game.load.image('heart', 'assets/img/corazon.png');
     game.load.audio('music', 'assets/sound/main.mp3');
 }
 
 function createMenu(){
-        var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
-        var styleInfo = { font: "bold 16px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        var style = { font: "bold 32px brain_flowerregular", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+        var styleInfo = { font: "bold 16px brain_flowerregular", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
         var gameTitle = game.add.text(175, 160, "Tweetfairy", style);
 		gameTitle.anchor.setTo(0.5,0.5);
         var gameInfo = game.add.text(175, 275, "Use the arrows to move", styleInfo);
