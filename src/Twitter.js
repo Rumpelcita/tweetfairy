262634d194ebca88a93bde403fe95f40723f17db
Florencia Rumpel Rodriguez <rumpelcita@gmail.com>
2016-02-01 12:32:41 -0300

    Update Twitter.js

--- src/Twitter.js
+++ src/Twitter.js
@@ -6,8 +6,8 @@ function Twitter() {
         buffs : [],
     }
     var cb = new Codebird;
-    cb.setConsumerKey("fna55bUkYfhEY9WAEh6JTy9yF", "B7i01p6fqMfHnDX5Nm7tROUnQRnTKCdNglK1Yx54f03afvIBFx");
-    cb.setToken("4865898394-DEDy93K13G7jF5GywQBrZ9pU0tPJ5uTPZKL1cc6", "mF1Yw4CKuCx0egwhLSSL6nSd8ZTteeCWIIvxGjpl1WYgH");
+    cb.setConsumerKey();
+    cb.setToken();
     cb.__call(
     "oauth2_token",
     {},
